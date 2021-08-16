bf8fd7181344551020209feba088fd71077c7ef1
RomTurino <romturino@gmail.com>
2021-08-16 21:30:52 +0300

    I created all models for the database

---
+++ studwork/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from .models import *
+
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('contract_number', 'course',
+                    'teacher', 'parent', 'student',
+                    'count_paid_lessons', 'contract_date')
+    list_display_links = ('course', 'teacher', 'parent',
+                          'student')
+    search_fields = ('course', 'teacher', 'parent',
+                     'student')
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('surname', 'name', 'curator', 'specialization')
+    list_display_links = ('name', 'surname')
+    search_fields = ('name', 'surname', 'curator', 'specialization')
+
+
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'administrator')
+    search_fields = ('name', 'administrator')
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+    search_fields = ('name', 'age')
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('lesson_date', 'lesson_type', 'lesson_time',
+                    'is_done', 'right_answers')
+    list_display_links = ('lesson_date', 'lesson_type')
+    search_fields = ('lesson_date', 'lesson_type', 'is_done')
+
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('code', 'content', 'question_count', 'grade')
+    list_display_links = ('code', 'content')
+    search_fields = ('code', 'content')
+
+
+class LessonThemeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'content', 'grade')
+    list_display_links = ('code', 'content')
+    search_fields = ('code', 'content', 'grade')
+
+
+class CoursePlanAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course_grade')
+    search_fields = ('title', 'course_grade')
+
+
+admin.site.register(Contract, ContractAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Parent, ParentAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Lesson)
+admin.site.register(Test, TestAdmin)
+admin.site.register(CoursePlan, CoursePlanAdmin)
+admin.site.register(LessonTheme, LessonThemeAdmin)
---
+++ studwork/models.py
@@ -0,0 +1,151 @@
+import uuid
+
+from django.db import models
+
+
+class Contract(models.Model):
+    contract_number = models.IntegerField(verbose_name='Номер договора',
+                                          primary_key=True)
+    teacher = models.OneToOneField('Teacher', on_delete=models.CASCADE,
+                                   related_query_name='teacher',
+                                   verbose_name='Преподаватель')
+    parent = models.OneToOneField('Parent', on_delete=models.CASCADE,
+                                  related_query_name='parent',
+                                  verbose_name='Родитель')
+    student = models.ForeignKey('Student', on_delete=models.CASCADE,
+                                related_query_name='student',
+                                verbose_name='Ученик')
+    lessons = models.ManyToManyField('Lesson', related_query_name='lesson',
+                                     verbose_name='Уроки')
+    count_paid_lessons = models.IntegerField(verbose_name='Количество купленных уроков')
+    contract_date = models.DateTimeField(verbose_name='Дата заключения',
+                                         auto_now_add=True)
+    course = models.ForeignKey('CoursePlan', on_delete=models.CASCADE,
+                               related_query_name='course',
+                               verbose_name='Курс')
+
+    class Meta:
+        verbose_name_plural = 'Договора'
+        verbose_name = 'Договор'
+        ordering = ['contract_date']
+
+    def __str__(self):
+        return f'{self.contract_number}. {self.teacher} - {self.parent}'
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя')
+    surname = models.CharField(max_length=150, verbose_name='Фамилия')
+    curator = models.ForeignKey('self', on_delete=models.SET_NULL,
+                                max_length=150, verbose_name='Куратор',
+                                null=True, blank=True)
+    specialization = models.CharField(max_length=150,
+                                      verbose_name='Специализация')
+
+    class Meta:
+        verbose_name_plural = 'Учители'
+        verbose_name = 'Учитель'
+        ordering = ['surname']
+
+    def __str__(self):
+        return f'{self.surname} {self.name[:1]}.'
+
+
+class Parent(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя')
+    administrator = models.OneToOneField(Teacher, on_delete=models.CASCADE,
+                                         verbose_name='Администратор')
+
+    class Meta:
+        verbose_name_plural = 'Родители'
+        verbose_name = 'Родитель'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Имя')
+    age = models.IntegerField(verbose_name='Возраст')
+
+    class Meta:
+        verbose_name_plural = 'Ученики'
+        verbose_name = 'Ученик'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    KINDS = (
+        ('s', 'Обычный'),
+        ('k', 'Контрольный')
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    lesson_type = models.CharField(max_length=1, choices=KINDS, default='s',
+                                   verbose_name='Обычный/контрольный')
+    lesson_themes = models.ManyToManyField('LessonTheme', related_query_name='themes',
+                                           verbose_name='Темы')
+    test = models.ManyToManyField('Test', related_query_name='test',
+                                  verbose_name='Тест')
+    lesson_time = models.TimeField(verbose_name='Время урока')
+    lesson_date = models.DateField(verbose_name="Дата урока")
+    is_done = models.BooleanField(verbose_name="Проведен")
+    right_answers = models.IntegerField(verbose_name="Количество правильных ответов")
+
+    class Meta:
+        verbose_name_plural = 'Уроки'
+        verbose_name = 'Урок'
+        ordering = ['lesson_date', 'lesson_type']
+
+    def __str__(self):
+        return f'{self.lesson_type}, {self.lesson_date} {self.lesson_time}'
+
+
+class Test(models.Model):
+    code = models.IntegerField(verbose_name='Код теста', primary_key=True)
+    question_count = models.IntegerField(verbose_name='количество вопросов')
+    content = models.TextField(verbose_name='Контент')
+    grade = models.IntegerField(verbose_name='Оценка', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Тесты'
+        verbose_name = 'Тест'
+        ordering = ['code']
+
+    def __str__(self):
+        return f'{self.code}'
+
+
+class CoursePlan(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название курса',
+                             primary_key=True)
+    course_grade = models.IntegerField(verbose_name='Оценка')
+    test = models.ManyToManyField(Test, related_query_name='plan_to_test',
+                                  verbose_name='Тест')
+    themes = models.ManyToManyField('LessonTheme', related_query_name='themes',
+                                    verbose_name='Темы')
+
+    class Meta:
+        verbose_name_plural = 'Планы курса'
+        verbose_name = 'План курса'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class LessonTheme(models.Model):
+    code = models.IntegerField(verbose_name='Код урока', primary_key=True)
+    content = models.TextField(verbose_name='Содержание')
+    grade = models.IntegerField(verbose_name='Оценка темы')
+
+    class Meta:
+        verbose_name_plural = 'Темы урока'
+        verbose_name = 'Тема урока'
+        ordering = ['content']
+
+    def __str__(self):
+        return f'{self.code}. {self.content[:10]}'
