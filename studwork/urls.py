41ab2aad9c4830999fe6407b6044e91c79ded7f6
RomTurino <romturino@gmail.com>
2021-08-26 09:43:18 +0300

    added teacher and parent detail page

--- studwork/urls.py
+++ studwork/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from studwork import views
+from . import views
 
 urlpatterns = [
     path('students/', views.all_students, name='students'),
     path('courses/', views.all_courses, name='courses'),
     path('course/<str:title>', views.one_course, name='course'),
-
+    path('student/<int:id>', views.one_student, name='stud_detail'),
+    path('parent/<int:id>', views.one_parent, name='parent'),
+    path('teacher/<int:id>', views.one_teacher, name='teacher')
 
 ]
--- studwork/views.py
+++ studwork/views.py
@@ -21,9 +21,51 @@ def all_courses(request):
 def one_course(request, title):
     course = CoursePlan.objects.get(title=title)
     courses = CoursePlan.objects.all()
-    contracts = Contract.objects.filter(course = title)
-    students = Student.objects.filter(id__in = contracts)
+    contracts = Contract.objects.filter(course=title)
+    students = Student.objects.filter(id__in=contracts)
     content = {'courses': courses,
                'course': course,
                'students': students}
     return render(request, 'studbase/all_courses.html', content)
+
+
+def one_student(request, id):
+    student = Student.objects.get(id=id)
+    contracts = Contract.objects.filter(student=id)
+    parent = Parent.objects.get(id__in=contracts)
+    lessons = Lesson.objects.filter(pk__in=contracts)
+    lesson_themes = LessonTheme.objects.filter(pk__in=lessons)
+    tests = Test.objects.filter(lesson_test__in=lessons)
+    content = dict(student=student,
+                   parent=parent,
+                   contracts=contracts,
+                   lessons=lesson_themes,
+                   tests=tests)
+    return render(request, 'studbase/student_detail.html', content)
+
+
+def one_parent(request, id):
+    parent = Parent.objects.get(id=id)
+
+    contracts = Contract.objects.filter(parent=id)
+    children = Student.objects.filter(id__in=contracts)
+    print(contracts)
+    content = dict(parent=parent,
+                   children=children)
+    return render(request, 'studbase/parent_detail.html', content)
+
+
+def one_teacher(request, id):
+    teacher = Teacher.objects.get(id=id)
+    contracts = Contract.objects.filter(teacher=id)
+    students = Student.objects.filter(id__in=contracts)
+    subordinates= Teacher.objects.filter(id=id)
+    if teacher.curator == teacher:
+        heading = 'Подчиненные'
+    else:
+        heading = 'Куратор'
+    content = dict(teacher=teacher,
+                   students=students,
+                   heading=heading,
+                   subordinates=subordinates)
+    return render(request, 'studbase/teacher_detail.html', content)
