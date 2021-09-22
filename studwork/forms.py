3a7766db1c88775feedb984c98a1f10028a86e50
RomTurino <romturino@gmail.com>
2021-09-22 12:57:48 +0300

    added required = False in right_answers field

--- studwork/forms.py
+++ studwork/forms.py
@@ -5,7 +5,7 @@ from .models import Lesson, LessonTheme
 class LessonForm(forms.ModelForm):
     lesson_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
     lesson_date = forms.DateField(widget=forms.SelectDateWidget(attrs={'type': 'date'}))
-    right_answers = forms.IntegerField(widget=forms.NumberInput(attrs={'min': 0, 'max': 100}))
+    right_answers = forms.IntegerField(widget=forms.NumberInput(attrs={'min': 0, 'max': 100}),required=False)
 
     class Meta:
         model = Lesson
